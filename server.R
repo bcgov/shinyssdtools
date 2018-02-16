b62f53f33fb2887770a946543128bad6284dc79a
Seb Dalgarno <seb@poissonconsulting.ca>
2018-02-16 12:11:34 -0800

    fit dist tab working with hint mesages and checks

--- server.R
+++ server.R
@@ -9,21 +9,9 @@ function(input, output, session) {
     upload_state = NULL
   )
   
-  observeEvent(input$uploadData, {
-    upload.values$upload_state <- 'uploaded'
-  })
-  
-  observeEvent(input$demoData, {
-    upload.values$upload_state <- 'demo'
-  })
-  
-  observeEvent(input$useHot, {
-    upload.values$upload_state <- 'hot'
-  })
-  
   read_data <- reactive({
     req(upload.values$upload_state)
-    if (upload.values$upload_state == 'uploaded') {
+    if (upload.values$upload_state == 'upload') {
       data <- input$uploadData
       if(!grepl(".csv", data$name, fixed = TRUE)) {
         Sys.sleep(1)
@@ -33,7 +21,7 @@ function(input, output, session) {
     } else if (upload.values$upload_state == 'demo') {
       return(readr::read_csv("demo-data/boron-data.csv"))
     } else if (upload.values$upload_state == 'hot') {
-      return()
+      return(hot_data())
     } })
   
   hot.values = reactiveValues()
@@ -43,42 +31,196 @@ function(input, output, session) {
       DF = hot_to_r(input$hot)
     } else {
       if (is.null(hot.values[["DF"]]))
-        DF = data.frame(Concentration = c(NA_real_, NA_real_, NA_real_, NA_real_, NA_real_), 
-                        Species = c(NA_character_, NA_character_, NA_character_, NA_character_, NA_character_))
+        DF = data.frame(Concentration = rep(NA_real_, 10), 
+                        Species = rep(NA_character_, 10),
+                        Group = rep(NA_character_, 10))
       else
         DF = hot.values[["DF"]]
     }
-    
     hot.values[["DF"]] = DF
     DF
   })
   
-  output$hot <- renderRHandsontable({
-    DF = hot_data()
-    if (!is.null(DF))
-      rhandsontable(DF, width = 600, )
-  })
-  # --- render column choices
-
-  # --- fit distributions
+  clean_data <- reactive({
+    data <- read_data()
+    if(length(data)) {
+      # remove any column names like X1, X2 (blank headers from excel/numbers)
+      data[,colnames(data) %in% paste0("X", 1:200)] <- NULL
+      # remove any rows with all NA
+      data <- data[!(rowSums(is.na(data) | data == "") == ncol(data)),]
+      # remove any cols with all NA
+      # data <- data[,colSums(is.na(data) | data == "") < nrow(data)]
+    }
+    print(data)
+      data
+      
+    })
   
+  check_fit <- reactive({
+    req(input$selectConc)
+    req(input$selectDist)
+    
+    conc <- input$selectConc
+    dist <- input$selectDist
+    
+    data <- clean_data()
+    
+    if(length(data[[conc]]) == 0L)
+      return("You have not added a dataset.")
+    if(!is.numeric(data[[conc]]))
+      return("Concentration column must contain numbers.")
+    if(any(is.na(data[[conc]])))
+      return("Concentration values must not be missing.")
+    if(any(data[[conc]] <= 0))
+      return("Concentration values must be positive.")
+    if(any(is.infinite(data[[conc]])))
+      return("Concentration values must be finite.")
+    if(length(unique(data[[conc]])) < 8)
+      return("There must be at least 8 distinct concentration values.")
+    if(is.null(dist))
+      return("At least one distribution mut be selected.")
+    ""
+  })
+  
+  output$hintFi <- renderText(check_fit())
+  output$hintPr <- renderText(check_fit())
+  output$hintFi <- renderText(check_fit())
+  
+  
+  names_data <- reactive({
+    data <- clean_data()
+    names(data) %<>% make.names
+    data
+  })
+  
+  # --- render column choices
+  column_names <- reactive({
+    names(clean_data())
+  })
+  
+  guess_conc <- reactive({
+    name <- column_names()
+    name[grepl("conc", name %>% tolower)][1]
+  })
+  
+  guess_spp <- reactive({
+    name <- column_names()
+    name[grepl( "sp", name %>% tolower)][1]
+  })
+  
+  # --- fit distributions
+  fit_dist <- reactive({
+    print(clean_data())
+    data <- names_data()
+    conc <- input$selectConc %>% make.names()
+    dist <-  ssdca::ssd_fit_dists(data, left = conc,
+                                          dists = input$selectDist, silent = TRUE)
+  })
+  
+  plot_dist <- reactive({
+    withProgress(message = "Calculating...", value = 0,{
+      incProgress(0.6)
+      ggplot2::autoplot(fit_dist())
+    })
+  })
+  
+  table_gof <- reactive({
+    gof <- ssdca::ssd_gof(fit_dist()) %>% dplyr::mutate_if(is.numeric, ~ round(., 2))
+  })
   # --- predict and model average
-
+  
   # --- create feedback
   
   ########### Outputs --------------------
+  # --- datasets
+  output$hot <- renderRHandsontable({
+    DF = hot_data()
+    if (!is.null(DF))
+      rhandsontable(DF, width = 600, useTypes = FALSE) 
+  })
+  
+  output$viewUpload <- renderDataTable({
+    read_data()
+  })
+  
   # --- render UI with choices based on file upload
+  output$selectConc = renderUI({
+    selectInput("selectConc", 
+                label = label_mandatory("Select column with concentration values:"), 
+                choices = column_names(),
+                selected = guess_conc())
+  })
+  
+  output$selectSpp = renderUI({
+    selectInput("selectSpp", 
+                label = "Label by:", 
+                choices = c("-none-", column_names()),
+                selected = guess_spp())
+  })
+  
+  output$selectGroup = renderUI({
+    selectInput("selectGroup", 
+                label = "Colour by:", 
+                choices = c("-none-", column_names()),
+                selected = "-none-")
+  })
+  
+  output$selectDist <- renderUI({
+    selectizeInput('selectDist', 
+                   label = label_mandatory("Select distributions to fit:"),
+                   multiple = TRUE, 
+                   choices = c(default.dists, extra.dists),
+                   selected = default.dists,
+                   options = list(
+                     'plugins' = list('remove_button'),
+                     'create' = TRUE,
+                     'persist' = FALSE))
+  })  
+  
+  output$estHc <- renderUI({HTML( paste0("<b>", describe_hc()$est, "</b>"))})
+  
+  # --- results
+  output$distPlot <- renderPlot({
+    if(check_fit() != "")
+      return(NULL)
+    plot_dist()
+  })
+  
+  output$gofTable <- renderDataTable({ 
+    if(check_fit() != "")
+      return(NULL)
+    datatable(table_gof(), options = list(paging = FALSE, sDom  = '<"top">lrt<"bottom">ip'))})
+  
+  # --- predict
+  output$modelAveragePlot <- renderPlot({
+    plot_model_average()
+  })
   
   # --- download handlers
   
   ########### Observers --------------------
+  # --- data upload
+  observeEvent(input$uploadData, {
+    upload.values$upload_state <- 'upload'
+  })
+  
+  observeEvent(input$demoData, {
+    upload.values$upload_state <- 'demo'
+  })
+  
+  observeEvent(input$hot, {
+    upload.values$upload_state <- 'hot'
+  })
   
   # --- describe results
   
   # --- feedback
   
   # --- information
- 
+  
 }
+
+
+
 
 
--- ui.R
+++ ui.R
@@ -12,44 +12,77 @@ fluidPage(
   
   tabsetPanel(type = "tabs",
               tabPanel(title = span(tagList(icon("table"), "1. Data")),
-                       h5("Choose one of the following options:"),
-                       hr(),
-                       inline(p("1. Use ")),
-                       inline(actionLink("demoData", label = "boron dataset", icon = icon('table'))),
-                       # upload csv with data
-                       hr(),
-                       p("2. Upload a csv dataset"),
-                       fileInput('uploadData', buttonLabel = span(tagList(icon("upload"), "csv")),
-                                 label = "", placeholder = "Upload your data...",
-                                 accept = c('.csv')),
-                       # input data in DataTable
-                       hr(),
-                       p("3. Fill out Spreadsheet"),
-                       rHandsontableOutput("hot"),
-                       hr(),
-                       h5("Preview chosen dataset:"),
-                       wellPanel(dataTableOutput('viewUpload'), style = "overflow-x:scroll; max-height: 600px")),
+                       fluidRow(
+                         column(5,
+                                h5("Choose one of the following options:"),
+                                hr(),
+                                inline(p("1. Use ")),
+                                inline(actionLink("demoData", label = "boron dataset", icon = icon('table'))),
+                                helpText("This can be used to demo the app or view a dataset in the correct format."),
+                                # upload csv with data
+                                hr(),
+                                p("2. Upload a csv file"),
+                                helpText("If you have an xls/xlsx file, try exporting a worksheet as csv from excel."),
+                                
+                                fileInput('uploadData', buttonLabel = span(tagList(icon("upload"), "csv")),
+                                          label = "", placeholder = "Upload your data...",
+                                          accept = c('.csv')),
+                                # input data in DataTable
+                                hr(),
+                                p("3. Fill out Spreadsheet"),
+                                helpText("Right-click on the table to delete/insert rows or columns.", "Column names cannot be changed.", "Species and Group are optional."),
+                                rHandsontableOutput("hot"),
+                                hr()),
+                         column(7,
+                                h5("Preview chosen dataset:"),
+                                hr(),
+                                wellPanel(dataTableOutput('viewUpload', width = 500), 
+                                          style = "overflow-x:scroll; max-height: 600px; max-width: 540px")))),
               tabPanel(title = span(tagList(icon("stats", lib = "glyphicon"), "2. Fit")),
-                       br(),
-                       inline(downloadButton("dlDistPlot", label = "plot", style = 'padding:4px; font-size:80%')),
-                       inline(downloadButton("dlGofTable", label = "table", style = 'padding:4px; font-size:80%')),
-                       br(), br(),
-                       plotOutput("distPlot"),
-                       br(),
-                       dataTableOutput("gofTable")),
+                       fluidRow(
+                         column(4,
+                                br(),
+                                helpText("Hint:"),
+                                wellPanel(
+                                  textOutput('hintFi')
+                                ),
+                                wellPanel(
+                                  uiOutput('selectConc'),
+                                  uiOutput('selectDist'))
+                                ),
+                                
+                         column(8,
+                                br(),
+                                inline(downloadButton("dlDistPlot", label = "plot", style = 'padding:4px; font-size:80%')),
+                                inline(downloadButton("dlGofTable", label = "table", style = 'padding:4px; font-size:80%')),
+                                plotOutput("distPlot"),
+                                dataTableOutput("gofTable")))),
               tabPanel(title = span(tagList(icon("calculator"), "3. Predict")), 
-                       br(),
-                       inline(downloadButton("dlModelPlot", label = "plot", style = 'padding:4px; font-size:80%')),
-                       inline(downloadButton("dlPredTable", label = "table", style = 'padding:4px; font-size:80%')),
-                       br(), br(),
-                       plotOutput("modelAveragePlot"),
-                       br(),
-                       inline(htmlOutput("text1")),
-                       inline(uiOutput("selectHc")),
-                       inline(htmlOutput("text2")),
-                       inline(htmlOutput("estHc"))),
+                       fluidRow(
+                         column(4,
+                                br(),
+                                helpText("Having trouble?"),
+                                wellPanel(
+                                  htmlOutput('hintPr')
+                                ),
+                                wellPanel(
+                                  uiOutput('selectSpp'),
+                                  uiOutput('selectGroup'),
+                                  numericInput("selectHc", label = "Hazard Concentration:", value = 5, min = 0, 
+                                               max = 99, step = 5, width = "70px"))),
+                       column(8,
+                              br(),
+                              inline(downloadButton("dlModelPlot", label = "plot", style = 'padding:4px; font-size:80%')),
+                              inline(downloadButton("dlPredTable", label = "table", style = 'padding:4px; font-size:80%')),
+                              plotOutput("modelAveragePlot"),
+                              br(),
+                              inline(htmlOutput("estHc"))))),
               tabPanel(title = span(tagList(icon("calculator"), "4. Confidence Interval")), 
-                       br()),
+                       br(),
+                       helpText("Having trouble?"),
+                       wellPanel(
+                         htmlOutput('hintCi')
+                       )),
               tabPanel(title = span(tagList(icon("code"), "Rcode")), 
                        br())
   )
