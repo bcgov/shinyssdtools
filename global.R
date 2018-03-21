11a239ac10b26cfcac7f832467eb0e2fa9ca9740
Seb Dalgarno <seb@poissonconsulting.ca>
2018-03-20 18:52:36 -0700

    add citations to tech info

--- global.R
+++ global.R
@@ -13,7 +13,7 @@ slackr::slackr_setup(config_file = "./.slackr")
 
 # objects
 tech.info <- "This webpage fits species sensitivity distributions to concentration data. 
-              The user is able to select more than one distribution and plot the individual fits. 
+              The user is able to select more than one distribution and plot the individual fits. <br/><br/>
 The columns in the goodness of fit table are the distribution (dist), the Anderson-Darling 
 statistic (ad), the Kolmogorov-Smirnov statistic (ks), the Cramer-von Mises statistic (cvm), 
 Akaike’s Information Criterion (aic), Akaike’s Information Criterion corrected for sample size (aicc), 
@@ -21,9 +21,13 @@ Bayesian Information Criterion (bic), the AICc difference (delta) and the AICc b
 The prediction is the model averaged (using aicc) 
 estimate of the fit. 
 The percent hazard concentration is the concentration of the chemical 
-which is predicted to affect that percent of the species tested.
-The webpage which was developed by Seb Dalgarno is powered by the ssdca R package by Joe Thorley and Carl Schwarz. 
-For more information see https://github.com/bcgov/ssdca."
+which is predicted to affect that percent of the species tested.<br/><br/>
+To cite package ‘ssdca’ in publications use:<br/>
+Joe Thorley and Carl Schwarz (2018). ssdca: Species Sensitivity
+Distributions. R package version 0.0.0.9025.<br/><br/>
+To cite the web app use:<br/>
+Seb Dalgarno (2018) ssdca-shiny: A shiny web app to analyse species sensitivity distributions. 
+Prepared by Poisson Consulting for the Ministry of the Environment, British Columbia. https://poissonconsulting.shinyapps.io/ssdca-shiny/"
 
 extra.dists <- "pareto"
 default.dists <- c("lnorm", "llog", "gompertz", "lgumbel", "gamma", "weibull")
