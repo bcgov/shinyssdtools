c60bc433100b69b6cefa2f3c3d4c329e0c40e6c9
Ayla Pearson <aylapearson23@gmail.com>
2021-03-17 11:48:48 -0700

    removing files that relate to the package portion of the app

--- R/run_app.R
+++
@@ -1,8 +0,0 @@
-#' Run Shiny Application
-#'
-#' @export
-run_ssdtools_app <- function() {
-  shiny::shinyAppDir(system.file("app", package = "shinyssdtools"),
-    options = c("launch.browser" = TRUE)
-  )
-}
