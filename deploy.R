169e51de9113e40608e32a9bfb818345ccd8cbef
Ayla Pearson <aylapearson23@gmail.com>
2021-03-17 14:46:12 -0700

    switching back to the proper deploy script for client's account

--- deploy.R
+++ deploy.R
@@ -1,5 +1,2 @@
-# rsconnect::deployApp(appDir = ".", account = "bcgov-env", 
-#                      appName = "ssdtools", forceUpdate = TRUE)
-
-rsconnect::deployApp(appDir = ".", account = "poissonconsulting", 
-                     appName = "ssdtools_test2", forceUpdate = TRUE)
+rsconnect::deployApp(appDir = ".", account = "bcgov-env",
+                     appName = "ssdtools", forceUpdate = TRUE)
