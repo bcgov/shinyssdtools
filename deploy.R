38e39b8f90ffebde6201201f799e60131c0f9fe6
Angeline Tillmanns <Angeline.Tillmanns@gov.bc.ca>
2021-03-19 14:23:24 -0700

    adding a test site to the deploy script

--- deploy.R
+++ deploy.R
@@ -1,2 +1,9 @@
-rsconnect::deployApp(appDir = ".", account = "bcgov-env",
+#deploy to test site first
+rsconnect::deployApp(appDir = ".", account = "bcgov-env", 
+                     appName = "ssdtools_test", forceUpdate = TRUE)
+
+
+#deploy to live site
+rsconnect::deployApp(appDir = ".", account = "bcgov-env", 
                      appName = "ssdtools", forceUpdate = TRUE)
+
