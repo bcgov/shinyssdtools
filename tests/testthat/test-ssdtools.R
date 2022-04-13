91af65f6337251958f9ee8ee02dbd71db8a516b6
Seb Dalgarno <seb.dalgarno@gmail.com>
2022-04-13 14:16:54 -0700

    fix failed tests

--- tests/testthat/test-ssdtools.R
+++ tests/testthat/test-ssdtools.R
@@ -3,10 +3,10 @@ test_that("ssd_hp and ssd_hc output table includes all dists", {
   y <- ssdtools::ssd_fit_dists(ssddata::ccme_boron)
 
   pred_hc <- ssd_hc_ave(y, 5, 10L)
-  expect_identical(pred_hc$dist, c("average", "llogis", "gamma", "lnorm"))
+  expect_identical(pred_hc$dist, c("average", ssdtools::ssd_dists_bcanz()))
   expect_true(all(pred_hc$percent == 5))
 
-  pred_hp <- ssd_hp_ave(y, 5, 10L)
-  expect_identical(pred_hp$dist, c("average", "llogis", "gamma", "lnorm"))
+  pred_hp <- ssd_hp_ave(y, 5, 1L)
+  expect_identical(pred_hp$dist, c("average", ssdtools::ssd_dists_bcanz()))
   expect_true(all(pred_hp$conc == 5))
 })
